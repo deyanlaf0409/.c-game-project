d4e3cfb7129fa686d4ae9219b9769c5d9acb7039
Deyan Lafchiev <111605228+deyanlaf0409@users.noreply.github.com>
2023-02-04 13:15:40 +0200

    Update Main.c

--- GameFiles/Main.c
+++ GameFiles/Main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>  //For system("cls")
 #include <conio.h>  //For kbhit()
 #include <time.h>  //For rand() functions
-#include <math.h>
 #define _WIN64_WINNT  _WIN64_WINNT_WIN10
 #define NTDDI_VERSION NTDDI_WIN10_22H2
 #include <SdkDdkVer.h>//Macros to future-proof the code
